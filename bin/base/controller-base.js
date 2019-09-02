76154261118aedf67aa0a7f9d8e30fc34541c56e
Jefferson <jeffersonacm.jz@gmail.com>
2019-09-01 21:55:12 -0300

    Created base for controller and repository

---
+++ bin/base/controller-base.js
@@ -0,0 +1,87 @@
+exports.post = async(repository, validationContract, req, res) => {
+    try {
+
+        let data = req.body;
+
+        if (!validationContract.isValid()) {
+            res.status(400).send({
+                message: 'Os dados enviados não estão corretos.',
+                validation: validationContract.error()
+            }).end();
+            return;
+        }
+
+        let resultado = await repository.create(data);
+        res.status(201).send(resultado);
+    } catch (err) {
+        console.log('error: ', err);
+        res.status(500).send({ message: 'Erro ao processar a requisição: ', error: err});
+    }
+}
+
+exports.put = async(repository, validationContract, req, res) => {
+    try {
+
+        let data = req.body;
+
+        if (!validationContract.isValid()) {
+            res.status(400).send({
+                message: 'Os dados enviados não estão corretos.',
+                validation: validationContract.error()
+            }).end();
+            return;
+        }
+
+        let resultado = await repository.update(req.params.id, data);
+        res.status(202).send(resultado);
+    } catch (err) {
+        console.log('error: ', err);
+        res.status(500).send({ message: 'Erro ao processar a requisição: ', error: err});
+    }
+}
+
+exports.get = async(repository, req, res) => {
+    try {
+        let data = await repository.getAll();
+        res.status(200).send(data);
+    } catch (err) {
+        console.log('error: ', err);
+        res.status(500).send({ message: 'Erro ao processar a requisição: ', error: err});
+    }
+}
+
+exports.getById = async(repository, req, res) => {
+    try {
+        let id = req.params.id;
+        if(id) {
+            let data = await repository.getById(id);
+            res.status(200).send(data);
+        } else {
+            res.status(400).send({
+                message: 'Não foi informado o parâmetro ID.',
+            }).end();
+        }
+
+    } catch (err) {
+        console.log('error: ', err);
+        res.status(500).send({ message: 'Erro ao processar a requisição: ', error: err});
+    }
+}
+
+exports.delete = async(repository, req, res) => {
+    try {
+        let id = req.params.id;
+        if(id) {
+            let data = await repository.delete(id);
+            res.status(200).send('Registro excluido com sucesso.');
+        } else {
+            res.status(400).send({
+                message: 'Não foi informado o parâmetro ID.',
+            }).end();
+        }
+
+    } catch (err) {
+        console.log('error: ', err);
+        res.status(500).send({ message: 'Erro ao processar a requisição: ', error: err});
+    }
+}
